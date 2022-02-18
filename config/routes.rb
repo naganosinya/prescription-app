f16b43a9bd7113a6b553435b2fcf6b25a3c8fadd
mizuhokono <mizuhokono@gmail.com>
2022-02-18 15:23:53 +0900

    投稿後の待機ページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  get '/microposts/create_after', to: 'microposts#create_after'
+  get '/microposts/success', to: 'microposts#success'
+  get '/microposts/failed', to: 'microposts#failed'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'microposts#top'
   resources :microposts
