8a237fa7c154993c4fb9ddcc16331bfcea0f07bf
naganosinya <cknumber5@gmail.com>
2022-02-18 11:40:12 +0900

    annotate設定

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,2 +1,12 @@
+# == Schema Information
+#
+# Table name: microposts
+#
+#  id         :bigint           not null, primary key
+#  allow      :boolean
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Micropost < ApplicationRecord
 end
